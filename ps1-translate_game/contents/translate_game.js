c733c9569d4e3cd3935f3ed7535da6dd55248f4b
Stephen Freiberg <safreiberg@gmail.com>
2012-02-28 15:02:41 -0500

    added game

---
+++ ps1-translate_game/contents/translate_game.js
@@ -0,0 +1,10 @@
+// This allows the Javascript code inside this block to only run when the page
+// has finished loading in the browser.
+$(function() {
+	var lang_to		= "English";
+	var lang_from		= "Spanish";
+	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
+
+	// Your code here
+
+    });
